401af9d1a0e1ae162ce9a7a060381af9407bd473
Natalka Danilenko <nata1danilenko12n@gmail.com>
2019-06-18 18:54:49 +0300

    changes for threads

--- src/Main.java
+++ src/Main.java
@@ -7,9 +7,10 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Start programm");
-
+        double result = 1;
         for (int i = 0; i < 100; i++) {
-            Thread thr = new Thread(new NewThread(),i+1 + " thr");
+            result = result * (i + 1);
+            Thread thr = new Thread(new NewThread(),i+1 + " thr" + "-" + result);
             thr.start();
         }
 
